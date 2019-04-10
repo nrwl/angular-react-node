fb26615c0fd2c5d8a8b5460c8337cdcd2fc28cd2
Jack Hsu <jack.hsu@gmail.com>
2019-04-10 10:46:32 -0400

    Updates react app component

--- apps/admin/src/app/app.tsx
+++ apps/admin/src/app/app.tsx
@@ -1,25 +1,30 @@
 import * as React from 'react';
 import { Component } from 'react';
+import axios from 'axios';
 
 import './app.css';
 import { Ticket } from '@mycompany/api-types';
 
 export class App extends Component<{}, { tickets: Ticket[] }> {
-  componentWillMount(): void {
-    fetch('/api/tickets').then(r => r.json()).then(tickets => {
-      this.setState({ tickets });
-    });
+  state = {
+    tickets: []
+  };
+
+  async componentWillMount() {
+    const { data: tickets } = await axios('/api/tickets');
+    this.setState({ tickets });
   }
 
   render() {
-    const tickets = (this.state && this.state.tickets) ? this.state.tickets.map(t => (
-      <li>{t.title} | {t.adminComment}</li>)) : [];
     return (
       <>
-        <mycompany-header title={'Tickets admin!'}/>
-
+        <mycompany-header title={'Tickets admin!'} />
         <ul>
-          {tickets}
+          {this.state.tickets.map(t => (
+            <li key={t.title}>
+              {t.title} | {t.adminComment}
+            </li>
+          ))}
         </ul>
       </>
     );
